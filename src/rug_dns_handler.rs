cdeea8ad4172828280f09520040ed9e394f89ef7
JEB-Bem <jeb_alex@outlook.com>
2026-08-20 17:10:43 +0800

    feat: RugDNS Resolver.

--- src/rug_dns_handler.rs
+++ src/rug_dns_handler.rs
@@ -1,26 +1,83 @@
-use hickory_server::server::{Request, RequestHandler, ResponseHandler, ResponseInfo};
-use hickory_server::net::runtime::Time;
 use crate::config::Config;
+use crate::rug_dns_resolver::RugDnsResolver;
+use hickory_server::{
+    server::{Request, RequestHandler, ResponseHandler, ResponseInfo},
+    authority::MessageResponseBuilder
+};
+use hickory_proto::{
+    op::ResponseCode,
+    rr::RecordType,
+};
 use tracing::{info, debug, warn, error};
-use data_encoding::HEXLOWER;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 pub struct RugDnsHandler {
+    config: Arc<RwLock<Config>>,
+    resolver: RugDnsResolver,
 }
 
 impl RugDnsHandler {
-    pub fn new(config: &Config) -> Self {
-        RugDnsHandler {  }
+    pub fn new(config: Arc<RwLock<Config>>, resolver: RugDnsResolver) -> Self {
+        Self {
+            config, 
+            resolver,
+        }
     }
 }
 
 #[async_trait::async_trait]
 impl RequestHandler for RugDnsHandler {
-    async fn handle_request<R: ResponseHandler, T: Time> (
+    async fn handle_request<R: ResponseHandler> (
         &self,
         request: &Request,
-        response_handle: R,
+        mut response_handle: R,
     ) -> ResponseInfo {
-        debug!("Received request: {:?}", HEXLOWER.encode(request.as_slice()));
-        unimplemented!();
+        debug!("Received request: {request:?}");
+        // 这里使用了 Deref trait，实际上 request: Request 被转换为了 MessageRequest
+        debug!("Building MessageResponseBuilder...");
+        let builder = MessageResponseBuilder::from_message_request(request);
+        let query = {
+            if request.queries().len() > 1 {
+                // RCODE reference: https://www.iana.org/assignments/dns-parameters#dns-parameters-6
+                warn!("There is not exactly one query entry in the request.");
+                debug!("Sending err msg with RCODE: NotImp.");
+                let response = builder.error_msg(request.header(), ResponseCode::NotImp);
+                return response_handle.send_response(response).await.unwrap();
+            }
+            request.queries()[0].original().to_owned()
+        };
+        
+        debug!("Send lookup query {query:?} to rug resolver");
+        match self.resolver.lookup(query).await {
+            Ok(mut resp) => {
+                resp.set_id(request.id());
+                let name_servers = resp
+                    .name_servers()
+                    .iter()
+                    .filter(|record| record.record_type() != RecordType::SOA);
+
+                let soa = resp
+                    .name_servers()
+                    .iter()
+                    .filter(|record| record.record_type() == RecordType::SOA);
+
+                let response = builder.build(
+                    resp.header().to_owned(),
+                    resp.answers(),
+                    name_servers,
+                    soa,
+                    resp.additionals()
+                );
+                debug!("Built response: {response:?}");
+                response_handle.send_response(response).await.unwrap()
+            }
+            Err(err) => {
+                let response = builder.error_msg(request.header(), ResponseCode::ServFail);
+                warn!("resolver lookup error: {err}");
+                debug!("Send response with RCODE: ServFail.");
+                response_handle.send_response(response).await.unwrap()
+            }
+        }
     }
 }
--- src/rug_dns_resolver.rs
+++ src/rug_dns_resolver.rs
@@ -1,7 +1,98 @@
-use hickory_resolver::{
-    config::{ResolverConfig, ServerGroup},
+use crate::config::Config;
+use hickory_client::client::DnssecClient;
+use hickory_proto::{
+    h2::{HttpsClientStream, HttpsClientStreamBuilder},
+    op::Query,
+    runtime::TokioRuntimeProvider,
+    rustls as hickory_rustls,
+    xfer::{DnsHandle, DnsRequestOptions, DnsResponse, FirstAnswer},
 };
+use rustls::ClientConfig;
+use tokio::sync::RwLock;
+use std::sync::Arc;
+use tracing::{debug, info};
+use anyhow::Result;
 
 pub struct RugDnsResolver {
-    bar: u8,
+    config: Arc<RwLock<Config>>,
+    client: DnssecClient,
+    options: DnsRequestOptions,
+}
+
+impl RugDnsResolver {
+    /// Constructs a new DNS Resolver
+    pub async fn init(config: Arc<RwLock<Config>>) -> Self {
+        let mut options = DnsRequestOptions::default();
+        options.use_edns = true;
+        options.edns_set_dnssec_ok = true;
+        options.max_request_depth = 26;
+        options.recursion_desired = true;
+        // options.case_randomization = true;
+        let provider = TokioRuntimeProvider::default();
+        let stream_builder = HttpsClientStreamBuilder::with_client_config(
+            Arc::new(hickory_rustls::client_config()),
+            provider
+        );
+        let conn = stream_builder.build(
+            "223.5.5.5:443".parse().unwrap(),
+            "223.5.5.5".into(),
+            "/dns-query".into()
+        );
+        let (client, bg) = DnssecClient::connect(conn).await.expect("connect with 223.5.5.5:443");
+        info!("Client built with target: 233.5.5.5");
+        tokio::spawn(bg);
+
+        Self {
+            config,
+            client,
+            options,
+        }
+    }
+    
+    /// A *classic* DNS query
+    pub async fn lookup(&self, query: Query) -> Result<DnsResponse> {
+        // TODO: implement proxy.
+        // TODO: add dns cache feature.
+        debug!("Received query: {query:?}");
+        Ok(self.client.lookup(query, self.options).first_answer().await?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config;
+    use std::str::FromStr;
+    use tokio::process::Command;
+    use hickory_proto::{op::Message, rr::{Name, RecordType, RData}};
+
+    async fn fetch_simple_case_data() -> (&'static str, String, String){
+        let domain = "fanyi.baidu.com.";
+        let mut dig = Command::new("dig");
+        dig.args(&["@223.5.5.5", "+https", "+short", domain]);
+        let output = dig.output().await.unwrap().stdout;
+        let mut lines = str::from_utf8(&output).unwrap().lines();
+
+        (domain, lines.next().unwrap().into(), lines.next().unwrap().into())
+    }
+    
+    #[tokio::test]
+    async fn test_lookup_a() {
+        crate::init_tracing();
+        let config = config::init_config(None).await;
+        let resolver = RugDnsResolver::init(config).await;
+        let (domain, cname, ip) = fetch_simple_case_data().await;
+        debug!("Result of `{domain}` - cname: {cname}, ip:{ip}");
+
+        let query = Query::query(Name::from_str(domain).unwrap(), RecordType::A);
+        let msg = resolver.lookup(query).await.unwrap().into_message();
+        assert_eq!(msg.answer_count(), 2);
+        let answers = msg.answers();
+        dbg!(answers);
+        assert_eq!(answers[1].name().to_lowercase(), Name::from_str(&cname).unwrap());
+        assert_eq!(answers[1].data(), &RData::A(ip.parse().unwrap()));
+    }
+    
+    // TODO: 通过修改头里面的是否进行 DNSSEC 验证和是否在失败后仍然传输数据等选项，
+    // 验证 Rug DNS 会正确的处理这些 flags
 }
